c91de4d7fb6ff570345cdaafd0405c390fdf1494
3105Dzua <106080372+3105Dzuaa@users.noreply.github.com>
2023-09-24 13:42:17 +0700

    Continuing Setup Map, Player

---
+++ Assets/Pixel Adventure 1/Assets/Scripts/BaseState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class BaseState
+{
+    //Lớp cơ sở, mình có thể điều hướng các states ở các state con
+    //Hoặc ở lớp Manager
+    public abstract void EnterState(StateManager stateManager);
+
+    public abstract void ExitState(StateManager stateManager);
+
+    public abstract void UpdateState(StateManager stateManager);
+
+    public abstract void FixedUpdate(StateManager stateManager);
+
+}
