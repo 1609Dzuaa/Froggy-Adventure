89213fd0ea70294077eae4ea99cbfe347ea0dd6b
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2023-12-10 00:27:37 +0700

    setup

---
+++ Assets/Scripts/Enemies/Bee/BeeManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeManager : MEnemiesManager
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override bool DetectedPlayer()
+    {
+
+        return false;
+    }
+}
