61117c585627033292e9fb59e5d8e3a4742b63c4
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2024-02-04 01:10:52 +0700

    fix bug tilemap collider

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Xử lý UI khi loose, win game
-/// Xử lý UI nâng cấp skill
+/// Xử lý UI nâng cấp skill (?)
 /// Xử lý Data Player khi chết
 /// Build Level 2 và Boss ở cuối Level
 /// Build Boss cơ bản = state pattern, bỏ qua Behavior Tree vì 0 còn thgian
@@ -25,7 +25,7 @@ public class GameManager : BaseSingleton<GameManager>
     {
         //Popdown các panel
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //StartCoroutine(SwitchToCurrentScene());
     }
 
     public void SwitchNextScene()
@@ -44,4 +44,15 @@ public class GameManager : BaseSingleton<GameManager>
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
         UIManager.Instance.TriggerAnimation(GameConstants.SCENE_TRANS_START);
     }
+
+    /*public IEnumerator SwitchToCurrentScene()
+    {
+        UIManager.Instance.PopDownCreditsPanel();
+        UIManager.Instance.PopDownSettingsPanel();
+        UIManager.Instance.TriggerAnimation(GameConstants.SCENE_TRANS_END);
+
+        yield return new WaitForSeconds(_delayTrans);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        UIManager.Instance.TriggerAnimation(GameConstants.SCENE_TRANS_START);
+    }*/
 }
